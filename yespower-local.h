9837883c87acd3b225941ee529e591b89fe72f1f
xxjayxx44 <93721015+xxjayxx44@users.noreply.github.com>
2025-03-07 22:53:53 -0600

    Update yespower-local.h

--- yespower-local.h
+++ yespower-local.h
@@ -1,7 +1,7 @@
 #ifndef YESPOWER_LOCAL_H
 #define YESPOWER_LOCAL_H
 
-#include "yespower.h"
+#include "yespower-1.0.1/yespower.h"
 
 int yespower_init_local(yespower_local_t *local);
 int yespower_free_local(yespower_local_t *local);
