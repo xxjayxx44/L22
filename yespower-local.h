620e6c4cd0b4230a2561511cef016317c3a5ec3e
xxjayxx44 <93721015+xxjayxx44@users.noreply.github.com>
2025-03-07 22:51:33 -0600

    Create yespower-local.h

---
+++ yespower-local.h
@@ -0,0 +1,9 @@
+#ifndef YESPOWER_LOCAL_H
+#define YESPOWER_LOCAL_H
+
+#include "yespower.h"
+
+int yespower_init_local(yespower_local_t *local);
+int yespower_free_local(yespower_local_t *local);
+
+#endif /* YESPOWER_LOCAL_H */
