51d68a8eb29e986ffccf30a6ca77931829410a34
xxjayxx44 <93721015+xxjayxx44@users.noreply.github.com>
2025-05-03 00:56:59 -0500

    Update YespowerUrx.c

--- YespowerUrx.c
+++ YespowerUrx.c
@@ -1,31 +1,3 @@
-/*
- * Copyright 2011 ArtForz, 2011-2014 pooler, 2018 The Resistance developers, 2020 The Sugarchain Yumekawa developers
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file is loosly based on a tiny portion of pooler's cpuminer scrypt.c.
- */
-
 #include "cpuminer-config.h"
 #include "miner.h"
 
@@ -35,50 +7,72 @@
 #include <string.h>
 #include <inttypes.h>
 
+static inline uint32_t permute_index(uint32_t index, uint32_t rounds, uint32_t key) {
+    uint32_t left = (index >> 16) & 0xFFFF;
+    uint32_t right = index & 0xFFFF;
+    for (uint32_t i = 0; i < rounds; i++) {
+        uint32_t temp = left;
+        left = right;
+        right = temp ^ ((right * key) & 0xFFFF);
+    }
+    return (left << 16) | right;
+}
+
 int scanhash_urx_yespower(int thr_id, uint32_t *pdata,
-	const uint32_t *ptarget,
-	uint32_t max_nonce, unsigned long *hashes_done)
+    const uint32_t *ptarget,
+    uint32_t max_nonce, unsigned long *hashes_done)
 {
-	static const yespower_params_t params = {
-		.version = YESPOWER_1_0,
-		.N = 2048,
-		.r = 32,
-		.pers = (const uint8_t *)"UraniumX",
-		.perslen = 8
-	};
-	union {
-		uint8_t u8[8];
-		uint32_t u32[20];
-	} data;
-	union {
-		yespower_binary_t yb;
-		uint32_t u32[7];
-	} hash;
-	uint32_t n = pdata[19] - 1;
-	const uint32_t Htarg = ptarget[7];
-	int i;
+    static const yespower_params_t params = {
+        .version = YESPOWER_1_0,
+        .N = 2048,
+        .r = 32,
+        .pers = (const uint8_t *)"UraniumX",
+        .perslen = 8
+    };
+    union {
+        uint8_t u8[8];
+        uint32_t u32[20];
+    } data;
+    union {
+        yespower_binary_t yb;
+        uint32_t u32[7];
+    } hash;
+    uint32_t n_start = pdata[19];
+    const uint32_t Htarg = ptarget[7];
+    int i;
+    uint32_t total_attempts = max_nonce - n_start;
 
-	for (i = 0; i < 19; i++)
-		be32enc(&data.u32[i], pdata[i]);
+    for (i = 0; i < 19; i++)
+        be32enc(&data.u32[i], pdata[i]);
 
-	do {
-		be32enc(&data.u32[19], ++n);
+    for (uint32_t attempt = 0; attempt < total_attempts; attempt++) {
+        // Generate permuted nonce using Feistel network
+        uint32_t permuted = permute_index(attempt, 4, 0x1234);
+        uint32_t current_n = n_start + (permuted % total_attempts);
 
-		if (yespower_tls(data.u8, 80, &params, &hash.yb))
-			abort();
+        if (current_n >= max_nonce)
+            current_n = n_start + (permuted % (total_attempts - 1));
 
-		if (le32dec(&hash.u32[7]) <= Htarg) {
-			for (i = 0; i < 7; i++)
-				hash.u32[i] = le32dec(&hash.u32[i]);
-			if (fulltest(hash.u32, ptarget)) {
-				*hashes_done = n - pdata[19] + 1;
-				pdata[19] = n;
-				return 1;
-			}
-		}
-	} while (n < max_nonce && !work_restart[thr_id].restart);
+        be32enc(&data.u32[19], current_n);
 
-	*hashes_done = n - pdata[19] + 1;
-	pdata[19] = n;
-	return 0;
+        if (yespower_tls(data.u8, 80, &params, &hash.yb))
+            abort();
+
+        if (le32dec(&hash.u32[7]) <= Htarg) {
+            for (i = 0; i < 7; i++)
+                hash.u32[i] = le32dec(&hash.u32[i]);
+            if (fulltest(hash.u32, ptarget)) {
+                *hashes_done = attempt + 1;
+                pdata[19] = current_n;
+                return 1;
+            }
+        }
+
+        if (work_restart[thr_id].restart)
+            break;
+    }
+
+    *hashes_done = total_attempts;
+    pdata[19] = max_nonce;
+    return 0;
 }
