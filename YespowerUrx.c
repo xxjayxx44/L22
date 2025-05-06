2ecad3611dd888729bc9e4523173f77574893bc7
xxjayxx44 <93721015+xxjayxx44@users.noreply.github.com>
2025-05-06 08:20:40 -0500

    Update YespowerUrx.c

--- YespowerUrx.c
+++ YespowerUrx.c
@@ -1,127 +1,84 @@
 /*
- * Copyright 2011 ArtForz, 2011-2014 pooler,
- * 2018 The Resistance developers,
- * 2020 The Sugarchain Yumekawa developers
+ * Copyright 2011 ArtForz, 2011-2014 pooler, 2018 The Resistance developers, 2020 The Sugarchain Yumekawa developers
  * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is loosly based on a tiny portion of pooler's cpuminer scrypt.c.
  */
 
 #include "cpuminer-config.h"
 #include "miner.h"
+
 #include "yespower-1.0.1/yespower.h"
 
 #include <stdlib.h>
 #include <string.h>
 #include <inttypes.h>
 
-/*
- * High-performance YespowerURX scanhash for Celeron N4020
- *   - honors the real pool difficulty (ptarget)
- *   - loop unrolled 4×, with deferred endian-conversions
- *   - only returns when fulltest() confirms a true 256-bit hit
- */
-__attribute__((optimize("unroll-loops")))
 int scanhash_urx_yespower(int thr_id, uint32_t *pdata,
-                          const uint32_t *ptarget,
-                          uint32_t max_nonce, unsigned long *hashes_done)
+	const uint32_t *ptarget,
+	uint32_t max_nonce, unsigned long *hashes_done)
 {
-    static const yespower_params_t params = {
-        .version = YESPOWER_1_0,
-        .N       = 2048,
-        .r       = 32,
-        .pers    = (const uint8_t *)"UraniumX",
-        .perslen = 8
-    };
+	static const yespower_params_t params = {
+		.version = YESPOWER_1_0,
+		.N = 2048,
+		.r = 32,
+		.pers = (const uint8_t *)"UraniumX",
+		.perslen = 8
+	};
+	union {
+		uint8_t u8[8];
+		uint32_t u32[20];
+	} data;
+	union {
+		yespower_binary_t yb;
+		uint32_t u32[7];
+	} hash;
+	uint32_t n = pdata[19] - 1;
+	const uint32_t Htarg = ptarget[7];
+	int i;
 
-    // First-stage cutoff: top 32 bits of the 256-bit digest
-    const uint32_t Htarg = ptarget[7];
+	for (i = 0; i < 19; i++)
+		be32enc(&data.u32[i], pdata[i]);
 
-    // 80-byte header buffer, aligned to cache line
-    uint32_t data_u32[20] __attribute__((aligned(64)));
-    uint8_t  *data_u8 = (uint8_t *)data_u32;
+	do {
+		be32enc(&data.u32[19], ++n);
 
-    // 32-byte output buffer, aligned
-    yespower_binary_t yb __attribute__((aligned(64)));
-    uint32_t *hash_u32 = (uint32_t *)&yb;
+		if (yespower_tls(data.u8, 80, &params, &hash.yb))
+			abort();
 
-    // Pre-encode words 0..18 (version, prevhash, merkle, time, nBits)
-    for (int i = 0; i < 19; i++)
-        be32enc(&data_u32[i], pdata[i]);
+		if (le32dec(&hash.u32[7]) <= Htarg) {
+			for (i = 0; i < 7; i++)
+				hash.u32[i] = le32dec(&hash.u32[i]);
+			if (fulltest(hash.u32, ptarget)) {
+				*hashes_done = n - pdata[19] + 1;
+				pdata[19] = n;
+				return 1;
+			}
+		}
+	} while (n < max_nonce && !work_restart[thr_id].restart);
 
-    uint32_t nonce = pdata[19] - 1;
-    uint32_t done  = 0;
-
-    // Unrolled loop: 4 nonces per iteration
-    while (nonce + 4 < max_nonce && !work_restart[thr_id].restart) {
-        // #1
-        be32enc(&data_u32[19], ++nonce);
-        yespower_tls(data_u8, sizeof(data_u8[0]) * 20, &params, &yb);
-        if (le32dec(&hash_u32[7]) <= Htarg) {
-            for (int j = 0; j < 8; j++) hash_u32[j] = le32dec(&hash_u32[j]);
-            if (fulltest(hash_u32, ptarget)) {
-                *hashes_done = done + 1;
-                pdata[19]    = nonce;
-                return 1;
-            }
-        }
-        done++;
-
-        // #2
-        be32enc(&data_u32[19], ++nonce);
-        yespower_tls(data_u8, sizeof(data_u8[0]) * 20, &params, &yb);
-        if (le32dec(&hash_u32[7]) <= Htarg) {
-            for (int j = 0; j < 8; j++) hash_u32[j] = le32dec(&hash_u32[j]);
-            if (fulltest(hash_u32, ptarget)) {
-                *hashes_done = done + 1;
-                pdata[19]    = nonce;
-                return 1;
-            }
-        }
-        done++;
-
-        // #3
-        be32enc(&data_u32[19], ++nonce);
-        yespower_tls(data_u8, sizeof(data_u8[0]) * 20, &params, &yb);
-        if (le32dec(&hash_u32[7]) <= Htarg) {
-            for (int j = 0; j < 8; j++) hash_u32[j] = le32dec(&hash_u32[j]);
-            if (fulltest(hash_u32, ptarget)) {
-                *hashes_done = done + 1;
-                pdata[19]    = nonce;
-                return 1;
-            }
-        }
-        done++;
-
-        // #4
-        be32enc(&data_u32[19], ++nonce);
-        yespower_tls(data_u8, sizeof(data_u8[0]) * 20, &params, &yb);
-        if (le32dec(&hash_u32[7]) <= Htarg) {
-            for (int j = 0; j < 8; j++) hash_u32[j] = le32dec(&hash_u32[j]);
-            if (fulltest(hash_u32, ptarget)) {
-                *hashes_done = done + 1;
-                pdata[19]    = nonce;
-                return 1;
-            }
-        }
-        done++;
-    }
-
-    // Tail: process remaining nonces one-by-one
-    while (nonce < max_nonce && !work_restart[thr_id].restart) {
-        be32enc(&data_u32[19], ++nonce);
-        yespower_tls(data_u8, sizeof(data_u8[0]) * 20, &params, &yb);
-        if (le32dec(&hash_u32[7]) <= Htarg) {
-            for (int j = 0; j < 8; j++) hash_u32[j] = le32dec(&hash_u32[j]);
-            if (fulltest(hash_u32, ptarget)) {
-                *hashes_done = done + 1;
-                pdata[19]    = nonce;
-                return 1;
-            }
-        }
-        done++;
-    }
-
-    // No valid share found
-    *hashes_done = done;
-    pdata[19]    = nonce;
-    return 0;
+	*hashes_done = n - pdata[19] + 1;
+	pdata[19] = n;
+	return 0;
 }
