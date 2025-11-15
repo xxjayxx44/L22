372502a8953773913fe38aa8f2a20c805e63b846
xxjayxx44 <93721015+xxjayxx44@users.noreply.github.com>
2025-11-15 13:30:48 -0600

    Refactor SHA-256 functions and constants
    Updated SHA-256 implementation for better performance and clarity.

--- sha2.c
+++ sha2.c
@@ -1,285 +1,171 @@
+/*
+ * Copyright 2011 ArtForz
+ * Copyright 2011-2013 pooler
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.  See COPYING for more details.
+ */
+
 #include "cpuminer-config.h"
-#include "miner.h"
+#include "miner.h"        /* brings in existing be32dec/be32enc, sha256d prototype */
+
 #include <string.h>
 #include <inttypes.h>
 
-/* COMPILER OPTIMIZATIONS */
-#ifdef __GNUC__
-#define ALWAYS_INLINE __attribute__((always_inline))
-#define HOT __attribute__((hot))
-#define COLD __attribute__((cold))
-#else
-#define ALWAYS_INLINE
-#define HOT
-#define COLD
-#endif
+/* --- Constants --- */
 
-/* ULTRA-FAST CONSTANTS - CACHE ALIGNED */
-static const uint32_t SHA256_H0[8] __attribute__((aligned(32))) = {
-    0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
-    0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
+static const uint32_t SHA256_H0[8] = {
+    0x6a09e667u, 0xbb67ae85u, 0x3c6ef372u, 0xa54ff53au,
+    0x510e527fu, 0x9b05688cu, 0x1f83d9abu, 0x5be0cd19u
 };
 
-static const uint32_t SHA256_K[64] __attribute__((aligned(64))) = {
-    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+static const uint32_t SHA256_K[64] = {
+    0x428a2f98u, 0x71374491u, 0xb5c0fbcfu, 0xe9b5dba5u,
+    0x3956c25bu, 0x59f111f1u, 0x923f82a4u, 0xab1c5ed5u,
+    0xd807aa98u, 0x12835b01u, 0x243185beu, 0x550c7dc3u,
+    0x72be5d74u, 0x80deb1feu, 0x9bdc06a7u, 0xc19bf174u,
+    0xe49b69c1u, 0xefbe4786u, 0x0fc19dc6u, 0x240ca1ccu,
+    0x2de92c6fu, 0x4a7484aau, 0x5cb0a9dcu, 0x76f988dau,
+    0x983e5152u, 0xa831c66du, 0xb00327c8u, 0xbf597fc7u,
+    0xc6e00bf3u, 0xd5a79147u, 0x06ca6351u, 0x14292967u,
+    0x27b70a85u, 0x2e1b2138u, 0x4d2c6dfcu, 0x53380d13u,
+    0x650a7354u, 0x766a0abbu, 0x81c2c92eu, 0x92722c85u,
+    0xa2bfe8a1u, 0xa81a664bu, 0xc24b8b70u, 0xc76c51a3u,
+    0xd192e819u, 0xd6990624u, 0xf40e3585u, 0x106aa070u,
+    0x19a4c116u, 0x1e376c08u, 0x2748774cu, 0x34b0bcb5u,
+    0x391c0cb3u, 0x4ed8aa4au, 0x5b9cca4fu, 0x682e6ff3u,
+    0x748f82eeu, 0x78a5636fu, 0x84c87814u, 0x8cc70208u,
+    0x90befffau, 0xa4506cebu, 0xbef9a3f7u, 0xc67178f2u
 };
 
-/* ULTRA-FAST ROTATE - NO FUNCTION CALL */
-#define ROTR(x, n) (((x) >> (n)) | ((x) << (32 - (n))))
-
-/* OPTIMIZED SHA-256 FUNCTIONS */
-#define CH(x, y, z)  (((x) & (y)) ^ (~(x) & (z)))
-#define MAJ(x, y, z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
-#define SIG0(x)      (ROTR(x, 2)  ^ ROTR(x, 13) ^ ROTR(x, 22))
-#define SIG1(x)      (ROTR(x, 6)  ^ ROTR(x, 11) ^ ROTR(x, 25))
-#define sig0(x)      (ROTR(x, 7)  ^ ROTR(x, 18) ^ ((x) >> 3))
-#define sig1(x)      (ROTR(x, 17) ^ ROTR(x, 19) ^ ((x) >> 10))
-
-/* ULTRA-FAST SHA256 COMPRESSION - FULLY UNROLLED */
-static ALWAYS_INLINE HOT void 
-sha256_compress(uint32_t state[8], const uint32_t M[16]) {
-    uint32_t W[64];
-    uint32_t a, b, c, d, e, f, g, h;
-    
-    /* UNROLLED MESSAGE SCHEDULE */
-    W[0] = M[0];   W[1] = M[1];   W[2] = M[2];   W[3] = M[3];
-    W[4] = M[4];   W[5] = M[5];   W[6] = M[6];   W[7] = M[7];
-    W[8] = M[8];   W[9] = M[9];   W[10] = M[10]; W[11] = M[11];
-    W[12] = M[12]; W[13] = M[13]; W[14] = M[14]; W[15] = M[15];
-    
-    /* UNROLLED EXPANSION - 48 ROUNDS */
-    W[16] = sig1(W[14]) + W[9]  + sig0(W[1])  + W[0];
-    W[17] = sig1(W[15]) + W[10] + sig0(W[2])  + W[1];
-    W[18] = sig1(W[16]) + W[11] + sig0(W[3])  + W[2];
-    W[19] = sig1(W[17]) + W[12] + sig0(W[4])  + W[3];
-    W[20] = sig1(W[18]) + W[13] + sig0(W[5])  + W[4];
-    W[21] = sig1(W[19]) + W[14] + sig0(W[6])  + W[5];
-    W[22] = sig1(W[20]) + W[15] + sig0(W[7])  + W[6];
-    W[23] = sig1(W[21]) + W[16] + sig0(W[8])  + W[7];
-    W[24] = sig1(W[22]) + W[17] + sig0(W[9])  + W[8];
-    W[25] = sig1(W[23]) + W[18] + sig0(W[10]) + W[9];
-    W[26] = sig1(W[24]) + W[19] + sig0(W[11]) + W[10];
-    W[27] = sig1(W[25]) + W[20] + sig0(W[12]) + W[11];
-    W[28] = sig1(W[26]) + W[21] + sig0(W[13]) + W[12];
-    W[29] = sig1(W[27]) + W[22] + sig0(W[14]) + W[13];
-    W[30] = sig1(W[28]) + W[23] + sig0(W[15]) + W[14];
-    W[31] = sig1(W[29]) + W[24] + sig0(W[16]) + W[15];
-    W[32] = sig1(W[30]) + W[25] + sig0(W[17]) + W[16];
-    W[33] = sig1(W[31]) + W[26] + sig0(W[18]) + W[17];
-    W[34] = sig1(W[32]) + W[27] + sig0(W[19]) + W[18];
-    W[35] = sig1(W[33]) + W[28] + sig0(W[20]) + W[19];
-    W[36] = sig1(W[34]) + W[29] + sig0(W[21]) + W[20];
-    W[37] = sig1(W[35]) + W[30] + sig0(W[22]) + W[21];
-    W[38] = sig1(W[36]) + W[31] + sig0(W[23]) + W[22];
-    W[39] = sig1(W[37]) + W[32] + sig0(W[24]) + W[23];
-    W[40] = sig1(W[38]) + W[33] + sig0(W[25]) + W[24];
-    W[41] = sig1(W[39]) + W[34] + sig0(W[26]) + W[25];
-    W[42] = sig1(W[40]) + W[35] + sig0(W[27]) + W[26];
-    W[43] = sig1(W[41]) + W[36] + sig0(W[28]) + W[27];
-    W[44] = sig1(W[42]) + W[37] + sig0(W[29]) + W[28];
-    W[45] = sig1(W[43]) + W[38] + sig0(W[30]) + W[29];
-    W[46] = sig1(W[44]) + W[39] + sig0(W[31]) + W[30];
-    W[47] = sig1(W[45]) + W[40] + sig0(W[32]) + W[31];
-    W[48] = sig1(W[46]) + W[41] + sig0(W[33]) + W[32];
-    W[49] = sig1(W[47]) + W[42] + sig0(W[34]) + W[33];
-    W[50] = sig1(W[48]) + W[43] + sig0(W[35]) + W[34];
-    W[51] = sig1(W[49]) + W[44] + sig0(W[36]) + W[35];
-    W[52] = sig1(W[50]) + W[45] + sig0(W[37]) + W[36];
-    W[53] = sig1(W[51]) + W[46] + sig0(W[38]) + W[37];
-    W[54] = sig1(W[52]) + W[47] + sig0(W[39]) + W[38];
-    W[55] = sig1(W[53]) + W[48] + sig0(W[40]) + W[39];
-    W[56] = sig1(W[54]) + W[49] + sig0(W[41]) + W[40];
-    W[57] = sig1(W[55]) + W[50] + sig0(W[42]) + W[41];
-    W[58] = sig1(W[56]) + W[51] + sig0(W[43]) + W[42];
-    W[59] = sig1(W[57]) + W[52] + sig0(W[44]) + W[43];
-    W[60] = sig1(W[58]) + W[53] + sig0(W[45]) + W[44];
-    W[61] = sig1(W[59]) + W[54] + sig0(W[46]) + W[45];
-    W[62] = sig1(W[60]) + W[55] + sig0(W[47]) + W[46];
-    W[63] = sig1(W[61]) + W[56] + sig0(W[48]) + W[47];
-    
-    /* INIT WORKING VARS */
-    a = state[0]; b = state[1]; c = state[2]; d = state[3];
-    e = state[4]; f = state[5]; g = state[6]; h = state[7];
-    
-    /* FULLY UNROLLED COMPRESSION - 64 ROUNDS */
-    #define R(i) \
-        uint32_t t1 = h + SIG1(e) + CH(e, f, g) + SHA256_K[i] + W[i]; \
-        uint32_t t2 = SIG0(a) + MAJ(a, b, c); \
-        h = g; g = f; f = e; e = d + t1; d = c; c = b; b = a; a = t1 + t2;
-    
-    R(0);  R(1);  R(2);  R(3);  R(4);  R(5);  R(6);  R(7);
-    R(8);  R(9);  R(10); R(11); R(12); R(13); R(14); R(15);
-    R(16); R(17); R(18); R(19); R(20); R(21); R(22); R(23);
-    R(24); R(25); R(26); R(27); R(28); R(29); R(30); R(31);
-    R(32); R(33); R(34); R(35); R(36); R(37); R(38); R(39);
-    R(40); R(41); R(42); R(43); R(44); R(45); R(46); R(47);
-    R(48); R(49); R(50); R(51); R(52); R(53); R(54); R(55);
-    R(56); R(57); R(58); R(59); R(60); R(61); R(62); R(63);
-    
-    #undef R
-    
-    /* UPDATE STATE */
-    state[0] += a; state[1] += b; state[2] += c; state[3] += d;
-    state[4] += e; state[5] += f; state[6] += g; state[7] += h;
+/* --- Rotate and SHA-256 logical functions --- */
+static inline uint32_t rotr32(uint32_t x, unsigned n) {
+    return (x >> n) | (x << (32 - n));
 }
 
-/* ULTRA-FAST SHA256 INIT */
-static ALWAYS_INLINE HOT void 
-sha256_init(uint32_t state[8]) {
-    state[0] = 0x6a09e667; state[1] = 0xbb67ae85;
-    state[2] = 0x3c6ef372; state[3] = 0xa54ff53a;
-    state[4] = 0x510e527f; state[5] = 0x9b05688c;
-    state[6] = 0x1f83d9ab; state[7] = 0x5be0cd19;
+#define CH(x,y,z)   (((x) & ((y) ^ (z))) ^ (z))
+#define MAJ(x,y,z)  (((x) & ((y) | (z))) | ((y) & (z)))
+
+static inline uint32_t SIG0(uint32_t x) {
+    return rotr32(x, 2) ^ rotr32(x, 13) ^ rotr32(x, 22);
+}
+static inline uint32_t SIG1(uint32_t x) {
+    return rotr32(x, 6) ^ rotr32(x, 11) ^ rotr32(x, 25);
+}
+static inline uint32_t sig0(uint32_t x) {
+    return rotr32(x, 7) ^ rotr32(x, 18) ^ (x >> 3);
+}
+static inline uint32_t sig1(uint32_t x) {
+    return rotr32(x, 17) ^ rotr32(x, 19) ^ (x >> 10);
 }
 
-/* OPTIMIZED TRANSFORM */
-static ALWAYS_INLINE HOT void 
-sha256_transform(uint32_t state[8], const uint32_t block[16], int swap) {
+/* --- Core compression for one 512-bit block --- */
+static void sha256_compress(uint32_t state[8], const uint32_t M[16]) {
+    uint32_t W[64], S[8], t1, t2;
+    int i;
+
+    /* 1. Prepare message schedule */
+    for (i = 0; i < 16; i++)
+        W[i] = M[i];
+    for (i = 16; i < 64; i++)
+        W[i] = sig1(W[i - 2]) + W[i - 7]
+             + sig0(W[i - 15]) + W[i - 16];
+
+    /* 2. Init working vars */
+    for (i = 0; i < 8; i++)
+        S[i] = state[i];
+
+    /* 3. 64 rounds */
+    for (i = 0; i < 64; i++) {
+        t1 = S[7] + SIG1(S[4]) + CH(S[4],S[5],S[6])
+           + SHA256_K[i] + W[i];
+        t2 = SIG0(S[0]) + MAJ(S[0],S[1],S[2]);
+        S[7] = S[6]; S[6] = S[5]; S[5] = S[4];
+        S[4] = S[3] + t1; S[3] = S[2];
+        S[2] = S[1]; S[1] = S[0];
+        S[0] = t1 + t2;
+    }
+
+    /* 4. Fold back */
+    for (i = 0; i < 8; i++)
+        state[i] += S[i];
+}
+
+/* --- Public SHA-256 API --- */
+void sha256_init(uint32_t state[8]) {
+    memcpy(state, SHA256_H0, 8 * sizeof(uint32_t));
+}
+
+/**
+ * Transform one 512-bit block.
+ * @param state   current hash state (8 words)
+ * @param block   16 big-endian 32-bit words
+ * @param swap    if non-zero, bytes of each word in block[] are swapped
+ */
+void sha256_transform(uint32_t state[8], const uint32_t block[16], int swap) {
     uint32_t M[16];
-    
     if (swap) {
-        /* UNROLLED BYTE SWAP */
-        M[0] = __builtin_bswap32(block[0]);  M[1] = __builtin_bswap32(block[1]);
-        M[2] = __builtin_bswap32(block[2]);  M[3] = __builtin_bswap32(block[3]);
-        M[4] = __builtin_bswap32(block[4]);  M[5] = __builtin_bswap32(block[5]);
-        M[6] = __builtin_bswap32(block[6]);  M[7] = __builtin_bswap32(block[7]);
-        M[8] = __builtin_bswap32(block[8]);  M[9] = __builtin_bswap32(block[9]);
-        M[10] = __builtin_bswap32(block[10]); M[11] = __builtin_bswap32(block[11]);
-        M[12] = __builtin_bswap32(block[12]); M[13] = __builtin_bswap32(block[13]);
-        M[14] = __builtin_bswap32(block[14]); M[15] = __builtin_bswap32(block[15]);
+        for (int i = 0; i < 16; i++)
+            M[i] = __builtin_bswap32(block[i]);
     } else {
-        /* DIRECT COPY */
-        M[0] = block[0];  M[1] = block[1];  M[2] = block[2];  M[3] = block[3];
-        M[4] = block[4];  M[5] = block[5];  M[6] = block[6];  M[7] = block[7];
-        M[8] = block[8];  M[9] = block[9];  M[10] = block[10]; M[11] = block[11];
-        M[12] = block[12]; M[13] = block[13]; M[14] = block[14]; M[15] = block[15];
+        memcpy(M, block, 16 * sizeof(uint32_t));
     }
-    
     sha256_compress(state, M);
 }
 
-/* ULTRA-FAST SHA256 FOR MINING - OPTIMIZED FOR 80-BYTE BLOCKS */
-void ALWAYS_INLINE HOT 
-sha256(const unsigned char *data, size_t len, unsigned char out32[32]) {
+/* --- One-shot SHA-256 for arbitrary-length input --- */
+void sha256(const unsigned char *data, size_t len, unsigned char out32[32]) {
     uint32_t state[8];
+    unsigned char block[64];
+    size_t left = len;
     const unsigned char *ptr = data;
-    
+
     sha256_init(state);
-    
-    /* FAST PATH FOR FULL BLOCKS */
-    while (len >= 64) {
+
+    /* process full blocks */
+    while (left >= 64) {
         uint32_t M[16];
-        /* UNROLLED BIG-ENDIAN DECODE */
-        M[0] = be32dec(ptr);     M[1] = be32dec(ptr + 4);
-        M[2] = be32dec(ptr + 8); M[3] = be32dec(ptr + 12);
-        M[4] = be32dec(ptr + 16); M[5] = be32dec(ptr + 20);
-        M[6] = be32dec(ptr + 24); M[7] = be32dec(ptr + 28);
-        M[8] = be32dec(ptr + 32); M[9] = be32dec(ptr + 36);
-        M[10] = be32dec(ptr + 40); M[11] = be32dec(ptr + 44);
-        M[12] = be32dec(ptr + 48); M[13] = be32dec(ptr + 52);
-        M[14] = be32dec(ptr + 56); M[15] = be32dec(ptr + 60);
-        
+        for (int i = 0; i < 16; i++)
+            M[i] = be32dec(ptr + 4*i);
         sha256_compress(state, M);
-        ptr += 64;
-        len -= 64;
+        ptr += 64; left -= 64;
     }
-    
-    /* FINAL BLOCK */
-    unsigned char block[64] = {0};
-    if (len > 0) memcpy(block, ptr, len);
-    block[len] = 0x80;
-    
-    /* HANDLE LENGTH */
-    if (len >= 56) {
+
+    /* final block + padding */
+    memset(block, 0, 64);
+    if (left) memcpy(block, ptr, left);
+    block[left] = 0x80;
+
+    if (left >= 56) {
         uint32_t M[16];
-        for (int i = 0; i < 16; i++) M[i] = be32dec(block + i * 4);
+        for (int i = 0; i < 16; i++)
+            M[i] = be32dec(block + 4*i);
         sha256_compress(state, M);
-        memset(block, 0, 56);
+        memset(block, 0, 64);
     }
-    
-    /* APPEND BIT LENGTH */
-    uint64_t bitlen = (uint64_t)(data + (ptr - data)) << 3;
-    for (int i = 0; i < 8; i++) {
-        block[63 - i] = (unsigned char)(bitlen >> (i * 8));
+
+    /* append bit-length */
+    uint64_t bitlen = (uint64_t)len << 3;
+    for (int i = 0; i < 8; i++)
+        block[63 - i] = (unsigned char)(bitlen >> (8 * i));
+
+    {
+        uint32_t M[16];
+        for (int i = 0; i < 16; i++)
+            M[i] = be32dec(block + 4*i);
+        sha256_compress(state, M);
     }
-    
-    /* FINAL COMPRESSION */
-    uint32_t M[16];
-    for (int i = 0; i < 16; i++) M[i] = be32dec(block + i * 4);
-    sha256_compress(state, M);
-    
-    /* UNROLLED OUTPUT */
-    be32enc(out32, state[0]); be32enc(out32 + 4, state[1]);
-    be32enc(out32 + 8, state[2]); be32enc(out32 + 12, state[3]);
-    be32enc(out32 + 16, state[4]); be32enc(out32 + 20, state[5]);
-    be32enc(out32 + 24, state[6]); be32enc(out32 + 28, state[7]);
+
+    /* output */
+    for (int i = 0; i < 8; i++)
+        be32enc(out32 + 4*i, state[i]);
 }
 
-/* ULTRA-FAST DOUBLE SHA256 */
-void ALWAYS_INLINE HOT 
-sha256d(unsigned char *hash, const unsigned char *data, int len) {
+/* --- Double SHA-256 (matches miner.h signature) --- */
+void sha256d(unsigned char *hash, const unsigned char *data, int len) {
     unsigned char tmp[32];
     sha256(data, (size_t)len, tmp);
     sha256(tmp, 32, hash);
-}
-
-/* MINING-SPECIFIC OPTIMIZATIONS */
-
-/* FAST SHA256 FOR 80-BYTE MINING BLOCKS */
-void ALWAYS_INLINE HOT 
-sha256_80(const unsigned char data[80], unsigned char out32[32]) {
-    uint32_t state[8];
-    uint32_t M[16];
-    
-    sha256_init(state);
-    
-    /* FIRST BLOCK - 64 BYTES */
-    M[0] = be32dec(data);     M[1] = be32dec(data + 4);
-    M[2] = be32dec(data + 8); M[3] = be32dec(data + 12);
-    M[4] = be32dec(data + 16); M[5] = be32dec(data + 20);
-    M[6] = be32dec(data + 24); M[7] = be32dec(data + 28);
-    M[8] = be32dec(data + 32); M[9] = be32dec(data + 36);
-    M[10] = be32dec(data + 40); M[11] = be32dec(data + 44);
-    M[12] = be32dec(data + 48); M[13] = be32dec(data + 52);
-    M[14] = be32dec(data + 56); M[15] = be32dec(data + 60);
-    sha256_compress(state, M);
-    
-    /* SECOND BLOCK - 16 BYTES + PADDING */
-    M[0] = be32dec(data + 64); M[1] = be32dec(data + 68);
-    M[2] = be32dec(data + 72); M[3] = be32dec(data + 76);
-    M[4] = 0x80000000;  M[5] = 0; M[6] = 0; M[7] = 0;
-    M[8] = 0; M[9] = 0; M[10] = 0; M[11] = 0;
-    M[12] = 0; M[13] = 0; M[14] = 0; M[15] = 640; /* 80*8 = 640 bits */
-    sha256_compress(state, M);
-    
-    /* OUTPUT */
-    be32enc(out32, state[0]); be32enc(out32 + 4, state[1]);
-    be32enc(out32 + 8, state[2]); be32enc(out32 + 12, state[3]);
-    be32enc(out32 + 16, state[4]); be32enc(out32 + 20, state[5]);
-    be32enc(out32 + 24, state[6]); be32enc(out32 + 28, state[7]);
-}
-
-/* BATCH PROCESSING - 4 NONCES AT ONCE */
-void ALWAYS_INLINE HOT 
-sha256d_batch4(unsigned char hashes[4][32], const unsigned char *base_data, 
-               const uint32_t nonces[4], int data_len) {
-    unsigned char blocks[4][128];
-    
-    /* PREPARE BLOCKS IN PARALLEL */
-    for (int i = 0; i < 4; i++) {
-        memcpy(blocks[i], base_data, data_len - 4);
-        be32enc(blocks[i] + data_len - 4, nonces[i]);
-    }
-    
-    /* PROCESS IN BATCH */
-    for (int i = 0; i < 4; i++) {
-        sha256d(hashes[i], blocks[i], data_len);
-    }
 }
